9cbd91506d28648bcc4a3a39383d0d488d091c57
TooniOlaniyan <asalamlatif@gmail.com>
2023-11-22 10:51:32 +0100

    footer

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,25 +4,24 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <nav className='flex-between padding-x padding-y border border-x-secondary-orange'>
-        <div>
-            <Link href='/'>ayanfedara</Link>
-        </div>
+    <nav className="flex-between padding-x padding-y border border-x-secondary-orange">
+      <div>
+        <Link href="/">ayanfedara</Link>
+      </div>
 
-        <div>
-            <ul>
-                {NavLinks.map((item) => (
-                    <Link href={item.href} key={item.key} >{item.text}</Link>
-                ))}
-            </ul>
-        </div>
+      
+        <ul className=" flex-between gap-10">
+          {NavLinks.map((item) => (
+            <Link className='capitalize' href={item.href} key={item.key}>
+              {item.text}
+            </Link>
+          ))}
+        </ul>
+     
 
-        <div>
-            {/* suscribe to newsletter */}
-        </div>
-
+      <div>{/* suscribe to newsletter */}</div>
     </nav>
-  )
+  );
 }
 
 export default NavBar
--- constants/index.ts
+++ constants/index.ts
@@ -1,26 +1,17 @@
 export const footerLinks = [
   {
-    title: "About",
+    title: "Menu",
     links: [
-      { title: "How it works", url: "/" },
+      { title: "How we work", url: "/" },
       { title: "Featured", url: "/" },
-      { title: "Partnership", url: "/" },
+      { title: "About", url: "/" },
       { title: "Bussiness Relation", url: "/" },
-    ],
-  },
-  {
-    title: "Company",
-    links: [
-      { title: "Events", url: "/" },
-      { title: "Blog", url: "/" },
-      { title: "Podcast", url: "/" },
-      { title: "Invite a friend", url: "/" },
     ],
   },
   {
     title: "Socials",
     links: [
-      { title: "Discord", url: "/" },
+      { title: "Teleram", url: "/" },
       { title: "Instagram", url: "/" },
       { title: "Twitter", url: "/" },
       { title: "Facebook", url: "/" },
