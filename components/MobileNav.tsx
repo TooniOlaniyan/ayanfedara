8a8f5996c5b3015d344a04e64fb86769d3ec2723
TooniOlaniyan <asalamlatif@gmail.com>
2023-11-30 12:47:19 +0100

    change

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+'use client'
+import {useEffect , useRef} from "react";
+import gsap from "gsap";
+import Link from "next/link";
+import Image from "next/image";
+import { NavLinks } from "@/constants";
+import Suscribe from "./Suscribe";
 
-const MobileNav = () => {
-  return (
-    <div>MobileNav</div>
-  )
+interface MobileMenuProps {
+  isOpen: boolean;
+  onClose: () => void;
 }
 
-export default MobileNav
+const MobileNav = ({isOpen , onClose} : MobileMenuProps) => {
+  const mobileMenuRef = useRef<HTMLDivElement>(null);
+  if(!isOpen){
+    return null
+  }
+
+  
+  return (
+    <div
+    ref={mobileMenuRef}
+      className="block fixed top-0 bottom-0 w-full h-full  sm:hidden"
+    >
+      <div onClick={onClose} className="absolute top-0 right-0 p-4">
+       <Image  src='/close.svg'  alt="Close" width={30} height={30} />
+      </div>
+      <div className="w-[100vw] flex flex-col items-center justify-around h-full bg-green-100">
+        <ul className="text-[2rem] text-white flex flex-col items-center justify-around  gap-10 uppercase">
+          {NavLinks.map((link) => (
+            <li key={link.key}>
+              <Link href={link.href}>
+                {link.text}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <Suscribe/>
+      </div>
+    </div>
+  );
+};
+
+export default MobileNav;
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,17 +1,21 @@
 'use client'
-
 import { NavLinks } from '@/constants'
 import Link from 'next/link'
-import React from 'react'
+import React , {useState} from 'react'
 import Image from 'next/image';
+import MobileNav from './MobileNav';
+
 
 const NavBar = () => {
-  const handleClick = () => {
-
+  const [isMobileOpen , setIsMobileOpen] = useState(false);
+  const handleMobileNavToggle = () => {
+    setIsMobileOpen(!isMobileOpen)
+    
   }
+ 
   return (
     <nav className="flex-between sticky top-0 z-50 padding-x padding-y backdrop-blur-[5px]">
-      <div className="flex-1">
+      <div  className="flex-1">
         <Link
           className="font-extrabold tracking-[5px] uppercase text-white"
           href="/"
@@ -25,9 +29,11 @@ const NavBar = () => {
         alt="hamburger"
         width={30}
         height={30}
-        className="block sm:hidden"
-        onClick={handleClick}
+        className="block cursor-pointer sm:hidden"
+        onClick={handleMobileNavToggle}
       />
+      <MobileNav isOpen={isMobileOpen} onClose={handleMobileNavToggle} />
+      
 
       <ul className="hidden md:flex flex-between !justify-around max-md:hidden md:gap-x-10 flex-1 gap-20 text-white rounded-3xl py-3 px-2 bg-gray-800 bg-opacity-40">
         {NavLinks.map((item) => (
